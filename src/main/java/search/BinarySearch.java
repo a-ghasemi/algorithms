e967517176b2c903432b8d355b5da3482128f6af
Armin <datarivers.org@gmail.com>
2024-01-09 10:25:31 +0100

    TSK-00001: implements binary search method [tests passed]

--- src/main/java/search/BinarySearch.java
+++ src/main/java/search/BinarySearch.java
@@ -1,16 +1,32 @@
 package search;
 
-import java.util.Collection;
+import static java.util.Arrays.sort;
 
-public class BinarySearch<T> implements Search<T> {
+public class BinarySearch<T extends Comparable<T>> implements Search<T> {
     T[] collection;
 
-    public BinarySearch(T[] collection){
+    public BinarySearch(T[] collection) {
         this.collection = collection;
     }
 
     @Override
     public Integer indexOf(T element) {
+        int low = 0;
+        int high = collection.length - 1;
+
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            int comparison = collection[mid].compareTo(element);
+
+            if (comparison > 0) {
+                high = mid - 1;
+            } else if (comparison < 0) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
         return null;
     }
 }
